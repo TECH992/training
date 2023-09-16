b687256ee188ac07e7a0b6e966ffeb90c4fd69fc
TECH992 <deepeshraj992@gmail.com>
2023-09-16 21:02:47 +0530

    router

---
+++ src/Page/NotFoundPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotFoundPage = () => {
+          return (
+                    <div>
+                              <h1 className='text-[24px] text-[#E21717] font-mono font-bold flex flex-row justify-center'>Sorry This page is not available</h1>
+                    </div>
+          )
+}
--- src/React-Router/AllRouters.js
+++ src/React-Router/AllRouters.js
@@ -3,13 +3,15 @@ import HomePage from '../Page/HomePage'
 import { Route, Routes } from 'react-router-dom';
 import ContactPage from '../Page/ContactPage';
 import About from '../Page/About';
+import { NotFoundPage } from '../Page/NotFoundPage';
 
 const AllRouters = () => {
           return (
                     <Routes>
                               <Route path="/" element={<HomePage />} /> 
                               <Route path="/contact" element={<ContactPage />} /> 
-                              <Route path="/about" element={<About />} /> 
+                              <Route path="/about" element={<About />} />
+                              <Route path='*' element={ <NotFoundPage/>} />
                     </Routes>
 
           )
